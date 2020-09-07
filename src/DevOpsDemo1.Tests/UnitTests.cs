e7d60bba110b280553e44906d688cd26df93df3a
Gerardo Garcia <g.garciac@outlook.com>
2020-09-07 13:03:40 -0500

    Pruebas

---
+++ src/DevOpsDemo1.Tests/UnitTests.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace DevOpsDemo1.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+        [Fact]
+        public void Test3()
+        {
+            Assert.True(true);
+        }
+        [Fact]
+        public void Test4()
+        {
+            Assert.True(true);
+        }
+    }
+}
